c2f13780e5bbe4f1ccc3adfe8805938f3e2604eb
Pedro Osternack Correa <pedro.oc@slalom.com>
2021-08-04 18:28:50 -0400

    Updating the codetour for the new repo format. And minor tweeks to the initial test.

--- src/Mastermind/Mastermind.spec.ts
+++ src/Mastermind/Mastermind.spec.ts
@@ -1,9 +1,11 @@
 import Mastermind from "./mastermind";
+import { CodeColours, HintColours } from "../Constants/Constants";
 
 describe("The Mastermind", function() {
   test("should initialize a secret code", function() {
-    var mastermind = new Mastermind();
+    var code = [CodeColours.RED, CodeColours.BLUE, CodeColours.BLUE, CodeColours.BLUE];
+    var mastermind = new Mastermind(code);
 
-    expect(mastermind.secretCode).not.toBe(null);
+    expect(mastermind.secretCode).toEqual(code);
   });
 });
